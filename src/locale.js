91a1ebd94d9a19b8fd48e131f577bc0debb7e689
Eric Ingram <eric@swell.is>
2021-02-01 19:46:22 -0700

    wip: multi-language 2

---
+++ src/locale.js
@@ -0,0 +1,48 @@
+const { find } = require('./utils');
+const { getCookie, setCookie } = require('./cookie');
+
+function methods(request, opt) {
+  return {
+    code: null,
+    state: null,
+
+    list() {
+      return opt.api.settings.get('store.locales', []);
+    },
+
+    async select(locale) {
+      this.set(locale);
+      setCookie('swell-locale', locale);
+      return await request('put', '/session', { locale });
+    },
+
+    selected() {
+      if (this.code) {
+        return this.code;
+      }
+      const storeLocale = opt.api.settings.get('store.locale');
+      const cookieLocale = getCookie('swell-locale');
+      return cookieLocale || storeLocale;
+    },
+
+    get() {
+      if (!this.code) {
+        this.code = this.selected();
+      }
+      if (!this.state) {
+        this.state = this.set(this.code);
+      }
+      return this.state;
+    },
+
+    set(code) {
+      this.code = code;
+      this.state = find(this.list(), { code }) || {};
+      return this.state;
+    },
+  };
+}
+
+module.exports = {
+  methods,
+};
