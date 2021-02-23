8d09686a9dd63b828ed22cdf5ef662d42cbccade
Greg Hoskin <greghoskin@Gregs-MacBook-Pro.local>
2021-02-23 14:41:30 -0600

    add rollup bundler

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import babel from "@rollup/plugin-babel";
+import pkg from "./package.json";
+
+const input = ["./src/api.js"];
+
+export default [
+  {
+    // UMD
+    input,
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: "bundled",
+      }),
+      terser(),
+    ],
+    output: {
+      file: `dist/${pkg.name}.min.js`,
+      format: "umd",
+      name: "Swell JS", // this is the name of the global object
+      esModule: false,
+      exports: "named",
+      sourcemap: true,
+    },
+  },// ESM and CJS
+  {
+    input,
+    plugins: [nodeResolve()],
+    output: [
+      {
+        dir: "dist/esm",
+        format: "esm",
+        exports: "named",
+        sourcemap: true,
+      },
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+  },
+];
