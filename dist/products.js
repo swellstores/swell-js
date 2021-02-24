e9136344632c835dbf19533a6f11b2fb9cae1910
Eric Ingram <eric@swell.is>
2021-02-24 16:38:05 -0700

    3.8.3

--- dist/products.js
+++ dist/products.js
@@ -234,6 +234,7 @@ function calculateVariation(input, options) {
       variation.sale_price = variant.sale_price || product.sale_price;
       variation.orig_price = variant.orig_price || product.orig_price;
       variation.stock_status = variant.stock_status || product.stock_status;
+      variation.images = variant.images && variant.images.length ? variant.images : product.images;
     }
   }
 
