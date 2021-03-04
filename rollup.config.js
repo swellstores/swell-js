b927d326f26d06320f15fecf77cca93275a57282
Greg Hoskin <greghoskin@Gregs-MacBook-Pro.local>
2021-03-04 09:35:51 -0600

    update rollup config to work w/ node

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
-import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
-import commonjs from '@rollup/plugin-commonjs';
 import del from 'rollup-plugin-delete';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import globals from 'rollup-plugin-node-globals';
+import builtins from 'rollup-plugin-node-builtins';
+import {
+  terser,
+} from 'rollup-plugin-terser';
+import pkg from './package.json';
 
 const input = ["./src/api.js"];
 
@@ -10,30 +15,38 @@ export default [
 // UMD, browser-friendly
 	{
 		input,
+    external: ['deepmerge', 'isomorphic-fetch', 'lodash', 'object-keys-normalizer', 'object-merge-advanced', 'qs', '@babel/runtime'],
 		output: {
 			name: 'swell-js',
 			file: pkg.browser,
 			format: 'umd'
 		},
 		plugins: [
-			del({ targets: 'dist/*' }),
-			resolve(),
-			commonjs({
-				include: 'node_modules/**',
-			}),
-			terser()
-		]
+      del({ targets: 'dist/*' }),
+      globals(),
+      builtins(),
+      resolve(),
+      terser(),
+      commonjs({ include: 'node_modules/**' }),
+      filesize(),
+    ]
 	},
   // CommonJS for Node,
   // ES modules for bundlers
   {
 		input,
-		// external: ['deepmerge', 'isomorphic-fetch', 'lodash', 'object-keys-normalizer', 'qs', '@babel/runtime'],
+		external: ['deepmerge', 'isomorphic-fetch', 'lodash', 'object-keys-normalizer', 'object-merge-advanced', 'qs', '@babel/runtime'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' },
 		],
-		
-    plugins: [commonjs(), resolve()],
+		plugins: [
+      globals(),
+      builtins(),
+      resolve(),
+      terser(),
+      commonjs({ include: 'node_modules/**' }),
+      filesize(),
+    ]
 	}
 ]
