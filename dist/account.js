17d5ee536163e3ad0a44bb2f4f7f95f17cf6fd6f
Greg Hoskin <greghoskin@Gregs-MacBook-Pro.local>
2021-09-14 13:08:52 -0700

    Revert "3.10.7"
    This reverts commit c3edffab5150051f2dff3f911e6cf10a0da53901.

--- dist/account.js
+++ dist/account.js
@@ -56,13 +56,6 @@ function methods(request) {
       return this.requestStateChange('put', '/account', data);
     },
     login: function login(email, password) {
-      if (password && password.password_token) {
-        return this.requestStateChange('post', '/account/login', {
-          email: email,
-          password_token: password.password_token
-        });
-      }
-
       return this.requestStateChange('post', '/account/login', {
         email: email,
         password: password
