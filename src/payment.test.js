fa08c9100e5119470364408c2cd6c47d3a26a496
Dima Zhukovsky <dima.zhukovsky@swell.is>
2023-02-01 17:38:40 +0000

    PayPal Commerce Platform

--- src/payment.test.js
+++ src/payment.test.js
@@ -1,9 +1,30 @@
-import { shouldUsePayPalEmail } from './payment';
+import { shouldUsePayPalEmail, loadScript } from './payment';
 import api from './api';
 
 describe('payment', () => {
   beforeEach(() => {
     api.init('test', 'pk_test');
+
+    // Mock document
+    global.headScripts = [];
+    global.document = {
+      head: {
+        appendChild: (script) => global.headScripts.push(script),
+      },
+      createElement: (tag) => {
+        const script = {
+          addEventListener(eventName, handler) {
+            if (eventName === 'load') {
+              setTimeout(handler, 100);
+            }
+          },
+          setAttribute(key, value) {
+            script[key] = value;
+          },
+        };
+        return script;
+      },
+    };
   });
 
   describe('shouldUsePayPalEmail', () => {
@@ -44,6 +65,44 @@ describe('payment', () => {
       );
 
       expect(usePayPalEmail).toBe(true);
+    });
+  });
+
+  describe('loadScript', () => {
+    it('should load script without attributes', async () => {
+      await loadScript(
+        'paypal-sdk',
+        'https://www.paypal.com/sdk/js?currency=USD&client-id=clientId&merchant-id=merchantId&intent=authorize&commit=false',
+      );
+      const script = global.headScripts.find(
+        (script) => script.id === 'paypal-sdk',
+      );
+      expect(script).toBeDefined();
+      expect(script.id).toEqual('paypal-sdk');
+      expect(script.src).toEqual(
+        'https://www.paypal.com/sdk/js?currency=USD&client-id=clientId&merchant-id=merchantId&intent=authorize&commit=false',
+      );
+    });
+    it('should load script with attributes', async () => {
+      await loadScript(
+        'paypal-sdk',
+        'https://www.paypal.com/sdk/js?currency=USD&client-id=clientId&merchant-id=merchantId&intent=authorize&commit=false',
+        {
+          'data-partner-attribution-id': 'SwellCommerce_SP',
+        },
+      );
+      const script = global.headScripts.find(
+        (script) => script.id === 'paypal-sdk',
+      );
+      expect(script).toBeDefined();
+      expect(script.id).toEqual('paypal-sdk');
+      expect(script.src).toEqual(
+        'https://www.paypal.com/sdk/js?currency=USD&client-id=clientId&merchant-id=merchantId&intent=authorize&commit=false',
+      );
+      expect(script).toHaveProperty(
+        'data-partner-attribution-id',
+        'SwellCommerce_SP',
+      );
     });
   });
 });
