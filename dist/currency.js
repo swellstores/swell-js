dd876d86bf6c6ea9fe76869ba7b61e2cb16100b1
Mike Pih <mike@swell.is>
2023-04-04 10:03:19 -0700

    3.21.5

--- dist/currency.js
+++ dist/currency.js
@@ -1,4 +1,4 @@
-export { m as default } from './currency-842f76f2.js';
+export { m as default } from './currency-42145ec0.js';
 import 'qs';
 import './index-512fc30d.js';
 import 'deepmerge';
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-export { a as default } from './api-8fbb5805.js';
+export { a as default } from './api-ee8d921e.js';
 import './card-91071403.js';
 import './index-512fc30d.js';
 import 'qs';
@@ -16,4 +16,4 @@ import './content-0afdcb05.js';
 import './settings-937b96f1.js';
 import './index-760c7caf.js';
 import './locale-4391bcf3.js';
-import './currency-842f76f2.js';
+import './currency-42145ec0.js';
