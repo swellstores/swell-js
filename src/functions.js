f60d95aa7e50eea78db3b8c477eb9c8241952fe7
Eric Ingram <eric@swell.is>
2023-11-03 15:20:50 -0600

    add comments, remove cookie state

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,14 @@
 function methods(request, _opt) {
   return {
+    /**
+     * Make a request to an app function and greceiveet a response
+     * @param {string} method
+     * @param {string} appId
+     * @param {string} functionName
+     * @param {any} data
+     * @param {object?} options
+     * @returns {any}
+     */
     request(method, appId, functionName, data, options = undefined) {
       return request(method, functionName, undefined, data, {
         ...options,
@@ -8,18 +17,50 @@ function methods(request, _opt) {
       });
     },
 
+    /**
+     * Helper to make a GET request to an app function and receive a response
+     * @param {string} appId
+     * @param {string} functionName
+     * @param {any} data
+     * @param {object?} options
+     * @returns {any}
+     */
     get(appId, functionName, data, options = undefined) {
       return this.request('get', appId, functionName, data, options);
     },
 
+    /**
+     * Helper to make a PUT request to an app function and receive a response
+     * @param {string} appId
+     * @param {string} functionName
+     * @param {any} data
+     * @param {object?} options
+     * @returns {any}
+     */
     put(appId, functionName, data, options = undefined) {
       return this.request('put', appId, functionName, data, options);
     },
 
+    /**
+     * Helper to make a POST request to an app function and receive a response
+     * @param {string} appId
+     * @param {string} functionName
+     * @param {any} data
+     * @param {object?} options
+     * @returns {any}
+     */
     post(appId, functionName, data, options = undefined) {
       return this.request('post', appId, functionName, data, options);
     },
 
+    /**
+     * Helper to make a DELETE request to an app function and receive a response
+     * @param {string} appId
+     * @param {string} functionName
+     * @param {any} data
+     * @param {object?} options
+     * @returns {any}
+     */
     delete(appId, functionName, data, options = undefined) {
       return this.request('delete', appId, functionName, data, options);
     },
--- src/session.js
+++ src/session.js
@@ -1,20 +1,26 @@
 function methods(request, opt) {
   return {
-    state: null,
-
+    /**
+     * Get the decoded session as an object of session values
+     */
     get() {
       return request('get', '/session');
     },
 
+    /**
+     * Get the encoded session cookie
+     * This simplifies storing or passing the session to another system
+     */
     getCookie() {
-      this.state = opt.getCookie('swell-session');
-      return this.state;
+      return opt.getCookie('swell-session');
     },
 
+    /**
+     * Set the encoded session cookie
+     * This simplifies restoring the session from another system
+     */
     setCookie(value) {
-      this.state = value;
       opt.setCookie('swell-session', value);
-      return this.state;
     },
   };
 }
