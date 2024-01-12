2d91d4d35271aa4d7b9bccf5442445a8dab2b524
Swell Mike <mike@swell.is>
2024-01-11 16:30:26 -0800

    cache-test: fix open handle w/ record timer

--- src/cache.test.js
+++ src/cache.test.js
@@ -1,14 +1,21 @@
 import api from './api';
 
 describe('cache', () => {
+  const details = { model: 'pages', id: '1' };
+
   beforeEach(() => {
     api.init('test', 'pk_test');
   });
 
-  describe('setRecord', () => {
+  afterEach(() => {
+    const { recordTimer } = api.cache.values(details);
+    recordTimer?.unref();
+  });
+
+  describe('#setRecord', () => {
     it('should not cause an infinite loop', async () => {
-      api.cache.set({ model: 'pages', id: '1', value: {} });
-      api.cache.setRecord(undefined, { model: 'pages', id: '1' });
+      api.cache.set({ ...details, value: {} });
+      api.cache.setRecord(undefined, details);
 
       const record = api.cache.get('pages', '1');
 
@@ -16,7 +23,7 @@ describe('cache', () => {
     });
   });
 
-  describe('getFetch', () => {
+  describe('#getFetch', () => {
     it('should request from API just once, when cache is enabled', async () => {
       api.cache.options.enabled = true;
       await api.content.get('pages', 'slug');
