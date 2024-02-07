bb1213fd18832963691a837e97df72614822e216
Vladislav Studenichnik <vstudenichnik@insoftgroup.com>
2024-02-07 15:17:39 +0100

    fix: use 'fetch' instead of script tag for vault requests

--- src/card.test.js
+++ src/card.test.js
@@ -1,40 +1,15 @@
-global.fetch = require('jest-fetch-mock');
-global.window = {};
-
 import api from './api';
 
-let VAULT_RESPONSE;
-
 describe('card', () => {
-  let script;
-
   beforeEach(() => {
     api.init('test', 'pk_test');
-    VAULT_RESPONSE = null;
-    // Mock document
-    global.document = {
-      createElement: (tag) => {
-        script = {
-          parentNode: {
-            removeChild: () => {},
-          },
-        };
-        setTimeout(() => {
-          window[`swell_vault_response_${window.__swell_vault_request_id}`]({
-            $data: VAULT_RESPONSE || { token: 't_test' },
-          });
-        }, 100);
-        return script;
-      },
-      getElementsByTagName: (tag) => {
-        return [{ appendChild: (el) => {} }];
-      },
-    };
   });
 
   describe('init', () => {
     describe('createToken', () => {
       it('should make request to POST /tokens', async () => {
+        fetch.mockResponse(JSON.stringify({ $data: { token: 't_test' } }));
+
         await api.card.createToken({
           number: '4242 4242 4242 4242',
           exp_month: 1,
@@ -42,65 +17,59 @@ describe('card', () => {
           cvc: 123,
         });
 
-        expect(script.src).toEqual(
-          'https://vault.schema.io/tokens?%24jsonp%5Bmethod%5D=post&%24jsonp%5Bcallback%5D=swell_vault_response_1&%24data%5Bnumber%5D=4242%204242%204242%204242&%24data%5Bexp_month%5D=1&%24data%5Bexp_year%5D=2099&%24data%5Bcvc%5D=123&%24key=pk_test',
+        expect(fetch).toHaveBeenCalledWith(
+          'https://vault.schema.io/tokens?%24jsonp%5Bmethod%5D=post&%24jsonp%5Bcallback%5D=none&%24data%5Bnumber%5D=4242%204242%204242%204242&%24data%5Bexp_month%5D=1&%24data%5Bexp_year%5D=2099&%24data%5Bcvc%5D=123&%24key=pk_test',
+          expect.objectContaining({
+            signal: expect.any(Object),
+          }),
         );
       });
 
       it('should throw an error if card cvc code is invalid', async () => {
-        try {
-          await api.card.createToken({
+        await expect(
+          api.card.createToken({
             cvc: 1,
-          });
-          throw new Error();
-        } catch (err) {
-          expect(err.message).toEqual('Card CVC code appears to be invalid');
-        }
+          }),
+        ).rejects.toThrow('Card CVC code appears to be invalid');
       });
 
       it('should throw an error if card expiry is invalid', async () => {
-        try {
-          await api.card.createToken({
+        await expect(
+          api.card.createToken({
             cvc: 123,
             exp_month: 100,
-          });
-          throw new Error();
-        } catch (err) {
-          expect(err.message).toEqual('Card expiry appears to be invalid');
-        }
+          }),
+        ).rejects.toThrow('Card expiry appears to be invalid');
       });
 
       it('should throw an error if card number is invalid', async () => {
-        try {
-          await api.card.createToken({
+        await expect(
+          api.card.createToken({
             cvc: 123,
             exp_month: 1,
             exp_year: 2099,
             number: '1',
-          });
-          throw new Error();
-        } catch (err) {
-          expect(err.message).toEqual('Card number appears to be invalid');
-        }
+          }),
+        ).rejects.toThrow('Card number appears to be invalid');
       });
 
       it('should throw an of vault request returns errors', async () => {
-        try {
-          VAULT_RESPONSE = {
-            errors: { gateway: { code: 'INVALID', message: 'Test error' } },
-          };
-          await api.card.createToken({
+        fetch.mockResponse(
+          JSON.stringify({
+            $data: {
+              errors: { gateway: { code: 'INVALID', message: 'Test error' } },
+            },
+          }),
+        );
+
+        await expect(
+          api.card.createToken({
             cvc: 123,
             exp_month: 1,
             exp_year: 2099,
             number: '4242 4242 4242 4242',
-          });
-          throw new Error();
-        } catch (err) {
-          expect(err.message).toEqual('Test error');
-          expect(err.param).toEqual('gateway');
-          expect(err.status).toEqual(402);
-        }
+          }),
+        ).rejects.toThrow('Test error');
       });
     });
 
